c6b631464c0dda0ff4e7e002399a7e0b1b9cdce9
aviv20-meet <aviv20@meet.mit.edu>
2020-08-02 19:16:27 +0300

    init

--- app/src/main/java/com/example/rozmana/MainActivity.java
+++ app/src/main/java/com/example/rozmana/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.rozmana;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.Btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, BeforeCalendar.class);
+                startActivity(intent);
+            }
+        });
     }
 }
